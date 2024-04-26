c578bfe400d423b5d98e8e69e0188f1414af49ce
Chaymae Elkhou <77268852+l3miage-elkhouc@users.noreply.github.com>
2024-04-26 16:22:58 +0200

    Update BureauRepository.java

--- flexOffice-backend/flexOffice-backend/src/main/java/com/soprasteria/flexOfficebackend/repository/BureauRepository.java
+++ flexOffice-backend/flexOffice-backend/src/main/java/com/soprasteria/flexOfficebackend/repository/BureauRepository.java
@@ -3,9 +3,9 @@ package com.soprasteria.flexOfficebackend.repository;
 import com.soprasteria.flexOfficebackend.model.Bureau;
 
 import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 
+/*  Définit les méthodes pour l'accès aux données des bureaux dans la base de données.*/
 public interface BureauRepository extends CrudRepository<Bureau, Integer> {
 
     Optional<Bureau> findByNom(String nom);
