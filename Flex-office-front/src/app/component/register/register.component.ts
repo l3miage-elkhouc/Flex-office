28fcb40bf4671f6d0a4101d07d148aecf24532d9
Chaymae Elkhou <77268852+l3miage-elkhouc@users.noreply.github.com>
2024-04-26 16:19:57 +0200

    Update register.component.ts

--- Flex-office-front/src/app/component/register/register.component.ts
+++ Flex-office-front/src/app/component/register/register.component.ts
@@ -29,31 +29,33 @@ export class RegisterComponent {
     if (this.registerForm.invalid) {
       return;
     }
-
+   // Récupérer les valeurs des champs email et password du formulaire
     const email = this.registerForm.value.email;
     const password = this.registerForm.value.password;
-
+    // Vérifier si l'email existe déjà dans la base de données
     this.userService.checkEmailExists(email).subscribe(
       exists => {
-        if (exists) {
+        if (exists) { // Si l'email existe, mettre à jour le mot de passe associé
           this.userService.updatePassword(email, password).subscribe(
             () => {
-
+              // Succès de la mise à jour du mot de passe
             },
             success => {
-              this.registrationError = false;
+              this.registrationError = false; // Pas d'erreur d'inscription
               this.registrationErrorMessage = 'Vous êtes bien enregistré !!';
                this.router.navigate(['/dashboard']); // Naviguer vers la page d'accueil
 
             }
           );
         } else {
+          // Si l'email n'existe pas dans la base de données
           this.registrationError = true;
           this.registrationErrorMessage = 'Cet email n\'existe pas dans la base de données.';
         }
       },
       error => {
-        this.registrationError = true;
+         // Gestion des erreurs lors de la vérification de l'email
+        this.registrationError = true; // Il y a une erreur d'inscription
         this.registrationErrorMessage = 'Une erreur s\'est produite lors de la vérification de l\'email.';
       }
     );
