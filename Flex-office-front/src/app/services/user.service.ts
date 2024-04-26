2e219d4e24287ad12959e3771ee57c2a27130461
Chaymae Elkhou <77268852+l3miage-elkhouc@users.noreply.github.com>
2024-04-26 16:40:37 +0200

    Update user.service.ts

--- Flex-office-front/src/app/services/user.service.ts
+++ Flex-office-front/src/app/services/user.service.ts
@@ -15,25 +15,30 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) { }
 
+  // Récupère tous les utilisateurs depuis le backend
   getUsers() {
     return this.httpClient.get(this.API_URL + this.ENDPOINT_USERS).pipe(
       catchError(this.handleError)
     );
   }
 
+  // Vérifie si un email existe déjà dans la base de données
   checkEmailExists(email: string) {
     return this.httpClient.get<boolean>(`${this.API_URL}/check-email/${email}`);
   }
 
+   // Envoie une requête de création d'utilisateur
   registerUser(email: string, password: string) {
     return this.httpClient.post<any>(`${this.API_URL}/register`, { email, password });
   }
-
+  
+  // Gère les erreurs HTTP en cas de problème lors de la communication avec le backend
   private handleError(error: HttpErrorResponse) {
     console.error('Une erreur s\'est produite:', error);
     return throwError('Une erreur s\'est produite, veuillez réessayer plus tard.');
   }
 
+  // Met à jour le mot de passe d'un utilisateur
   updatePassword(email: string, password: string): Observable<any> {
     const url = `${this. API_URL}/utilisateurs`;
     const body = { email: email, password: password };
